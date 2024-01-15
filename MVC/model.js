fb2388906e2f7b4ee3feb7f47749fc6b084c86d7
Benjamin Curry <benjamincurry777@gmail.com>
2024-01-15 16:08:31 +0000

    4-api-articles-id

--- MVC/model.js
+++ MVC/model.js
@@ -1,4 +1,5 @@
-const fs = require("fs/promises");
+const fs = require('fs/promises');
+const format = require('pg-format');
 
 class Model {
 
@@ -12,6 +13,7 @@ class Model {
         // Otherwise deconstruction will cause the function to loose its relationship with the Model object.
         this.fetchAllTopics = this.fetchAllTopics.bind(this);
         this.fetchAllEndpoints = this.fetchAllEndpoints.bind(this);
+        this.fetchArticleByID = this.fetchArticleByID.bind(this);
     }
 
     async fetchAllTopics() {
@@ -21,6 +23,22 @@ class Model {
 
     async fetchAllEndpoints() {
         return JSON.parse(await fs.readFile(Model.#endpointsPath, 'utf8'));
+    }
+
+    async fetchArticleByID(id) {
+
+        try {
+            const { rows:article } = await this.#db.query(`SELECT * FROM articles WHERE article_id=$1`, [id]);
+            if(article.length === 0) {
+                return Promise.reject({status: '404', msg: `No article was found with the id ${id}`})
+            }
+    
+            return article[0];
+
+        } catch(err) {
+            return Promise.reject(err);
+        }
+
     }
 }
 
--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const Controller = require('./MVC/controller');
 const app = express();
 const apiRouter = express.Router();
 const topicsRouter = express.Router();
+const articlesRouter = express.Router();
 
 const model = new Model(db);
 const controller = new Controller(model);
@@ -13,14 +14,23 @@ const controller = new Controller(model);
 app.use(express.json());
 app.use('/api', apiRouter);
 apiRouter.use('/topics', topicsRouter);
+apiRouter.use('/articles', articlesRouter);
 
 apiRouter.get('/', async (req, res, next) => controller.getEndpoints(req, res, next));
 topicsRouter.get('/', async(req, res, next) => await controller.getTopics(req, res, next));
+articlesRouter.get('/:article_id', async(req, res, next) => await controller.getArticle(req, res, next))
 
 // Error handling.
 app.use((err, req, res, next) => {
-    if (err.status === 400) {
-        res.status(400).send(err)
+    if (err.code === '22P02') {
+        res.status(400).send({msg: 'Bad Request'})
+    }
+    next(err)
+})
+
+app.use((err, req, res, next) => {
+    if (err.status === '404') {
+        res.status(404).send({msg: err.msg})
     }
     next(err)
 })
