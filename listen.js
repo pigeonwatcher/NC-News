8e07d247a84e14530aa5e771037a2e2fadd60b25
Benjamin Curry <benjamincurry777@gmail.com>
2024-01-18 13:14:59 +0000

    refactor

--- listen.js
+++ listen.js
@@ -1,7 +1,5 @@
 const app = require('./app');
 
 // if production enviroment, run the server.
-if(process.env.production) {
-    const { PORT = 9090 } = process.env;
-    app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
-}
+const { PORT = 9090 } = process.env;
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
